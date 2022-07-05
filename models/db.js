aaa9e5f371507b83d5d623ff3a70b7865cc704b5
Bissixp <fernandosbissi18@hotmail.com>
2022-07-05 13:25:10 -0300

    Correção db.js

--- models/db.js
+++ models/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: 'db',
-  user: 'root',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
   port: 3306,
-  password: 'password',
+  password: process.env.MYSQL_PASSWORD,
   database: 'StoreManager',
 });
 
